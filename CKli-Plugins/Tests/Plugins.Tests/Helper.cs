835b0cbac6a878cd249b60b99de364dbaa1b145a
Olivier Spinelli <olivier.spinelli@signature-code.com>
2026-09-02 14:22:21 +0200

    Use CKliRootEnv.SecretsStore instead of a new store.

--- CKli-Plugins/Tests/Plugins.Tests/Helper.cs
+++ CKli-Plugins/Tests/Plugins.Tests/Helper.cs
@@ -17,21 +17,19 @@ internal static class Helper
     /// <summary>
     /// Gets the GitHub provider through the https://github.com/CK-Build/CKli repository itself and caches it.
     /// This fails (<see cref="Assume.That(bool, FormattableString, string)"/>) if the GITHUB_CK_BUILD is not available
-    /// in the <see cref="DotNetUserSecretsStore"/> (that is used by these tests).
+    /// in this <see cref="CKliRootEnv.InstanceName"/> "CKli-CKli-Test" user secrets store: this uses the central <see cref="CKliRootEnv.SecretsStore"/>.
     /// </summary>
     /// <returns></returns>
     public static GitHostingProvider GetGitHubHostingProvider()
     {
         if( _gitHubProvider == null )
         {
-            // Using the real store here: the PAT must be locally registered for these tests to run.
-            var store = new DotNetUserSecretsStore();
             // The CKli repository is public.
             // The hosting provider is public (new repositories will be public by default).
             // But, to call GitHub, it is better to always use a PAT because anonymous API calls
             // have a low rate limit: AlwaysUseAuthentication is true for GitHub.
             // => To challenge the Read credentials, we must actually consider the ToPrivateAccessKey() instance.
-            var gitKey = new GitRepositoryKey( store, new Uri( "https://github.com/CK-Build/CKli" ), isPublic: true );
+            var gitKey = new GitRepositoryKey( CKliRootEnv.SecretsStore, new Uri( "https://github.com/CK-Build/CKli" ), isPublic: true );
             gitKey.AccessKey.PrefixPAT.ShouldBe( "GITHUB_CK_BUILD" );
             _gitHubProvider = (HttpGitHostingProvider?)gitKey.AccessKey.HostingProvider;
             _gitHubProvider.ShouldNotBeNull();
